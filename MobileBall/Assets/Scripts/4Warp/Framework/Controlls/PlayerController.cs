2bb8693c156bcf5cabf65b972ac2f337063f0ca5
Annonator <andreaspohl89@gmail.com>
2016-01-31 20:23:34 +0100

    Added Player Controller to Test ReSharper Configuration

---
+++ MobileBall/Assets/Scripts/4Warp/Framework/Controlls/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+namespace _4Warp.Framework.Controlls
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public void Start()
+        {
+            var test = new ArrayList(10);
+        }
+
+        public void Update()
+        {
+        }
+    }
+}
